e91d053fd0f471fefb8ad36213c16ddd71c84f92
Axa Pickering <axapickering@gmail.com>
2024-05-28 10:45:14 -0700

    got backend connected to mongodb

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/records", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
