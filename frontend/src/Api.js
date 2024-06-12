cec223f91a097abdac70330bdef0ec1f484d6efb
Axa Pickering <axapickering@gmail.com>
2024-06-11 19:24:29 -0700

    finished api.js in frontend, made changes to frontend to support jwt tokens

---
+++ frontend/src/Api.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
+
+/** API Class.
+ * Static class tying together methods used to get/send to to the API.
+ */
+
+class TrainerAppApi {
+
+  static token = "";
+
+  static async request(endpoint, data = {}, method = "GET") {
+    const url = new URL(`${BASE_URL}/${endpoint}`);
+    const headers = {
+      authorization: `Bearer ${this.token}`,
+      'content-type': 'application/json',
+    };
+
+    url.search = (method === "GET")
+      ? new URLSearchParams(data).toString()
+      : "";
+
+    // set to undefined since the body property cannot exist on a GET method
+    const body = (method !== "GET")
+      ? JSON.stringify(data)
+      : undefined;
+
+    const resp = await fetch(url, { method, body, headers });
+
+    //fetch API does not throw an error, have to dig into the resp for msgs
+    if (!resp.ok) {
+      console.error("API Error:", resp.statusText, resp.status);
+      const { error } = await resp.json();
+
+      if (Array.isArray(error.message)) {
+        throw error.message;
+      }
+      else {
+        console.log(error.message);
+        throw [error.message];
+      }
+
+    }
+
+    return await resp.json();
+  }
+
+  // User API routes
+
+  /**Takes user data from signup form calls api to register the user, returns
+   * response
+   */
+  static async signup(userData) {
+    let res = await this.request('users/signup', userData, "POST");
+    return res;
+  }
+
+  /**Takes username and password from login form and signs in the user via APi call */
+  static async login(userData) {
+    let res = await this.request('users/login', userData, "POST");
+    return res;
+  }
+
+  /**Takes username and calls API to get the rest of the user data */
+  static async getUserInfo(username) {
+    let res = await this.request(`users/${username}`);
+    return res.user;
+  }
+
+  // /** Takes in userdata and calls API to update user's data with new values*/
+  // static async update({ username, firstName, lastName, email }) {
+  //   let res = await this.request(`users/${username}`,
+  //     { firstName, lastName, email },
+  //     "PATCH");
+  //   return res.user;
+  // }
+
+}
+
+export default TrainerAppApi;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { BrowserRouter, Routes, Route , Navigate } from 'react-router-dom';
@@ -9,14 +10,68 @@ import TrainerDash from "./TrainerDash/TrainerDash";
 import HomePage from './HomePage';
 import WorkoutBuilder from './WorkoutBuilder/WorkoutBuilder';
 import ExerciseLibrary from './ExerciseLibrary';
+import TrainerAppApi from './Api';
+import { jwtDecode } from 'jwt-decode';
+import  userContext from './userContext';
+
 
 axios.defaults.withCredentials = true;
 
 const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(function () {
+    async function fetchUserInfo() {
+      if (token) {
+        localStorage.setItem("token", token)
+        TrainerAppApi.token = token;
+        const userInfo = jwtDecode(token);
+        try {
+          let user = await TrainerAppApi.getUserInfo(userInfo.username);
+          setUser(user);
+        } catch (err) {
+          console.error(err);
+        }
+      } else {
+        localStorage.clear();
+        setUser(null);
+      }
+      setIsloading(false);
+    }
+
+    fetchUserInfo();
+
+  }, [token]);
+
+  /**Function to be called in signup form to call API and register user */
+  async function signup(formData) {
+    let res = await TrainerAppApi.signup(formData);
+    setToken(res.token);
+    return;
+  }
+
+  /**Function to be called in login form to call API and login the user*/
+  async function login(formData) {
+    let res = await TrainerAppApi.login(formData);
+    setToken(res.token);
+    return;
+  }
+
+   /**Function to logout a user, resets user state to null */
+   function logout() {
+    setToken(null);
+  }
+
+  if (isLoading) return (<div><h1>Loading...</h1></div>);
+
+
   return (
     <div className="App" style={{ textAlign: "center"}}>
+      <userContext.Provider value={null}>
       <BrowserRouter>
         <NavBar />
         <Routes>
@@ -28,6 +83,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
+      </userContext.Provider>
     </div>
   );
 }
